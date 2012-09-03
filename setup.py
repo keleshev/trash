d8b90f94a8ee75a8f87874408b1d2e225dd6d6a9
Vladimir Keleshev <halst@ya.ru>
2012-09-03 22:27:35 +0200

    Make project installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""`trash` lives on `GitHub <http://github.com/halst/trash/>`_."""
+from distutils.core import setup
+
+
+setup(name='trash',
+      version='0.1.0',
+      description='Safe `rm` substitute for OS X',
+      long_description=__doc__,
+      license='MIT',
+      author='Vladimir Keleshev',
+      url='https://github.com/halst/trash/',
+      classifiers=['Intended Audience :: Developers',
+                   'Environment :: Console',
+                   'Programming Language :: Python :: 2',
+                   'Operating System :: MacOS',
+                   'License :: OSI Approved :: MIT License'],
+      keywords='rm, rmtrash, trash',
+      py_modules=['trash'],
+      scripts=['trash'])
